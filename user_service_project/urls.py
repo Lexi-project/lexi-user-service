7bd0a02e3eb7ddd828163e3bf9aab0c5909d21ff
anup-an <friek_an@yahoo.com>
2023-08-18 11:42:23 +0300

    Add endpoints for getting jwt tokens and refreshing tokens

--- user_service_project/urls.py
+++ user_service_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('userapp.urls'))
+    path('api/user/', include('userapp.urls'))
 ]
--- userapp/urls.py
+++ userapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('health-check', views.check_health, name='register'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(),
+         name='get-access-and-refresh-token'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='refresh-token'),
 ]
